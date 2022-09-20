e1a2151eb33b49e004a10641a7b4d18c38b7161a
emib1 <ibeman851@gmail.com>
2022-09-20 10:49:23 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const navbar = document.querySelector('.navbar')
+const bottomContainer = document.querySelector('.bottom-container')
+
+console.log(navbar.offsetHeight)
+window.addEventListener('scroll', () => {
+    if (window.scrollY > bottomContainer.offsetTop - navbar.offsetHeight - 50) { navbar.classList.add('active') }
+    else {
+        navbar.classList.remove('active')
+    }
+
+
+})
